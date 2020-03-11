0e96a38ed0aad5395659515796e795daba44cbfc
Roland Levy <rolandjlevy@gmail.com>
2020-03-11 17:53:34 +0000

    Front-end validation for contact form

---
+++ public/contact-validation.js
@@ -0,0 +1,9 @@
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const submitButton = document.querySelector('#submit');
+const contactForm = document.querySelector('.contactForm');
+
+contactForm.addEventListener('keyup', (e) => {
+  submitButton.disabled = nameInput.value || emailInput.value ? false : true;
+});
+
